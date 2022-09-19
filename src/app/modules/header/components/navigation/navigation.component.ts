f309c1bc6145ce822d36614cb7263ec1561af84f
Casper Bastholm <42714453+casperb123@users.noreply.github.com>
2022-09-19 08:44:29 +0200

    The navigation now closes when you click on a button on mobile

--- src/app/modules/header/components/navigation/navigation.component.ts
+++ src/app/modules/header/components/navigation/navigation.component.ts
@@ -1,5 +1,13 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map, Observable, Subscription } from 'rxjs';
 import { AuthenticationService } from 'src/app/modules/authentication/utilities/authentication.service';
 
 @Component({
@@ -7,7 +15,9 @@ import { AuthenticationService } from 'src/app/modules/authentication/utilities/
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit, OnDestroy {
+  private routerSub: Subscription;
+
   public isLoggedIn$: Observable<boolean>;
 
   @Input()
@@ -16,12 +26,25 @@ export class NavigationComponent implements OnInit {
   @Output()
   public navigationToggle: EventEmitter<boolean> = new EventEmitter();
 
-  constructor(private authService: AuthenticationService) {}
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.isLoggedIn$ = this.authService
       .getUserLoggedIn()
       .pipe(map((user) => !!user && !!user.token));
+
+    this.routerSub = this.router.events
+      .pipe(
+        filter(
+          (event) => event instanceof NavigationEnd && window.innerWidth <= 500
+        )
+      )
+      .subscribe(() => {
+        this.closeNavigation();
+      });
   }
 
   public openNavigation(): void {
@@ -34,5 +57,15 @@ export class NavigationComponent implements OnInit {
 
   public logout(): void {
     this.authService.logout();
+
+    if (window.innerWidth <= 500) {
+      this.closeNavigation();
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSub) {
+      this.routerSub.unsubscribe();
+    }
   }
 }
